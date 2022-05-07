8f98b2cf825270ab770cf1085738a5bde4e0761a
Milan-Joshi <joshimilan2310@gmail.com>
2022-05-07 15:44:32 -0700

    Initial Setup

---
+++ src/main/java/Billing.java
@@ -0,0 +1,14 @@
+public class Billing {
+
+
+
+    public static void main(String[] args) {
+        //Inventory build
+        //Card data build
+        //Input build
+
+        //validation
+
+    }
+
+}
